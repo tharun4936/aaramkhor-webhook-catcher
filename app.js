ffa758a35c03f3fcb52bc2183658e023ec2a69db
tharun4936 <tharun4936@gmail.com>
2021-11-11 16:40:18 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import { populateFillingSheet, getRawOrdersData, googleSpreadsheetInit } from './helpers.js'
+
+const app = express();
+
+const port = process.env.PORT || 3002;
+
+app.use(express.json())
+
+app.post('/webhooks/orders/created', async function (req, res) {
+    try {
+        const data = getRawOrdersData(req.body)
+        if (data.errors) throw new Error(data.errors)
+        const doc = await googleSpreadsheetInit();
+        await populateFillingSheet(doc, data);
+        res.status(200).send();
+    } catch (err) {
+        res.status(404).send();
+    }
+
+})
+
+app.post('/webhooks/orders/fulfilled', async function (req, res) {
+    try {
+        const data = req.body;
+        console.log('Showing all the fulfilled orders...')
+        console.log(data);
+        res.status(200).send()
+    } catch (err) {
+        console.log(err.message)
+        res.status(404).send(err)
+    }
+})
+
+app.post('/webhooks/sms/incoming', function (req, res) {
+    try {
+        console.log(req);
+        res.status(200).send()
+    } catch (err) {
+
+    }
+})
+
+app.post('/webhooks/sms/status', function (req, res) {
+    try {
+        console.log(req.body);
+        res.status(200).send()
+
+    } catch (err) {
+
+    }
+})
+
+app.post('/webhooks/sms/status/fallback', function (req, res) {
+    try {
+        console.log(req.body);
+        res.status(200).send()
+
+    } catch (err) {
+
+    }
+})
+
+app.listen(port, function () {
+    console.log("Server is running at port " + port);
+})
---
+++ helpers.js
@@ -0,0 +1,96 @@
+import dotenv from 'dotenv'
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+
+dotenv.config();
+
+const { SPREADSHEET_ID, TRACKING_LINK } = process.env;
+
+export const populateFillingSheet = async function (doc, data) {
+    try {
+        await doc.loadInfo()
+        const workspaceSheet = doc.sheetsByTitle[process.env.FILLING_SHEET_NAME];
+        let items = "";
+        let itemsQuant = "";
+        for (let i = 0; i < data.items.length; i++) {
+            items += data.items[i].order;
+            items += ' ~ ';
+            itemsQuant += String(data.items[i].quantity);
+            itemsQuant += ' ~ '
+        }
+        items = items.slice(0, -2);
+        itemsQuant = itemsQuant.slice(0, -2)
+        await workspaceSheet.addRow({
+            Order_Number: data.order_id,
+            Order: items,
+            Order_Quantity: itemsQuant,
+            Customer_Name: data.name,
+            Customer_Phone: String(data.phone),
+            Customer_Email: data.contact_email,
+            Tracking_Number: '',
+            Created_At: data.created_at,
+            Tracking_Link: TRACKING_LINK
+        })
+
+    } catch (err) {
+        throw err;
+    }
+}
+
+export const getRawOrdersData = function (requestBody) {
+    try {
+        let contact_email;
+        let phone;
+        let order_id;
+        const items = [];
+
+        const { line_items, shipping_address, created_at } = requestBody;
+        if (requestBody.customer.last_order_name)
+            order_id = requestBody.customer.last_order_name.slice(1);
+        else
+            order_id = String(requestBody.id);
+
+        if (requestBody.contact_email) contact_email = requestBody.contact_email;
+        else contact_email = "Not Provided!"
+
+        const name = shipping_address.name
+
+        if (shipping_address.phone) phone = shipping_address.phone
+        else phone = "Not Provided!";
+
+        const dateArr = created_at.split('T')[0].split('-');
+        const date = `${dateArr[2]}/${dateArr[1]}/${dateArr[0]}`;
+        const time = created_at.split('T')[1];
+
+        line_items.forEach(item => {
+            items.push({ order: item.title, quantity: item.quantity })
+        });
+
+        return {
+            order_id,
+            name,
+            items,
+            contact_email,
+            phone,
+            created_at: `${date} : ${time}`
+        }
+
+    } catch (err) {
+        throw err;
+    }
+
+}
+
+
+export const googleSpreadsheetInit = async function () {
+    try {
+        const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+        await doc.useServiceAccountAuth({
+            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        });
+        await doc.loadInfo();
+        return doc;
+    } catch (err) {
+        throw err;
+    }
+}
